ace76dd4c34c395a7154287e87cd8725d671ca17
Benjamin <bemmamin@live.dk>
2021-05-24 20:48:00 +0200

    Observer import

--- app/src/main/java/com/beehive/beerrate/ui/preference/PreferenceFragment.kt
+++ app/src/main/java/com/beehive/beerrate/ui/preference/PreferenceFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.observe
 import androidx.recyclerview.widget.DividerItemDecoration
